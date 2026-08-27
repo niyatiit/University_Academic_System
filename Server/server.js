1fc57f2c097a2ef73133b01fd2f64ef4827f50f5
niyati patel <niyatipatel0701@gmail.com>
2026-08-27 14:13:39 +0530

    Restrict CORS to production frontend domain

--- Server/server.js
+++ Server/server.js
@@ -21,7 +21,13 @@ connectDB()
 const app = express();
 
 //middlwares use
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://university-academic-system-six.vercel.app",
+  ],
+  credentials: true,
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
 
