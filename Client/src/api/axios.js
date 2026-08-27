ea859ecadaa0ce7581c31750c98764110f94a449
niyati patel <niyatipatel0701@gmail.com>
2026-08-27 14:03:38 +0530

    update the axios file after the backend deploye

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://university-academic-system.onrender.com/api",
 });
 
 // Attach token automatically to every request (if logged in)
